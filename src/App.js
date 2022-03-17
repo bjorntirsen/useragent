2fedcf1690a3f1acce1c2fa0db708fc6502f7c0f
Björn Tirsén <bjorntirsen@gmail.com>
2022-03-17 11:37:51 +0100

    Improve app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import './App.css';
 
 function App() {
-  console.log(navigator.userAgentData)
-  const userAgentData = navigator.userAgentData
+  if (navigator.userAgentData) {
+
+    console.log(navigator.userAgentData)
+    const userAgentData = navigator.userAgentData
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h4>Brands</h4>
+          {userAgentData.brands && userAgentData.brands.map(item => (
+            <p>{item.brand}: {item.version}</p>
+          ))}
+          <h4>Mobile</h4>
+          <p>{userAgentData.mobile && userAgentData.mobile.toString()}</p>
+          <h4>Platform</h4>
+          <p>{userAgentData.platform && userAgentData.platform}</p>
+        </header>
+      </div>
+    );
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <h4>Brands</h4>
-        {userAgentData.brands.map(item => (
-          <p>{item.brand}: {item.version}</p>
-        ))}
-        <h4>Mobile</h4>
-        <p>{userAgentData.mobile.toString()}</p>
-        <h4>Platform</h4>
-        <p>{userAgentData.platform}</p>
+        <h4>No User Agent was found</h4>
       </header>
     </div>
   );
