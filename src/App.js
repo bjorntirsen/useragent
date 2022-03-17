fafb46795aa24afb7a40ba40c338f069e8165e58
Björn Tirsén <bjorntirsen@gmail.com>
2022-03-17 11:23:31 +0100

    Add data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log(navigator.userAgentData)
+  const userAgentData = navigator.userAgentData
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h4>Brands</h4>
+        {userAgentData.brands.map(item => (
+          <p>{item.brand}: {item.version}</p>
+        ))}
+        <h4>Mobile</h4>
+        <p>{userAgentData.mobile.toString()}</p>
+        <h4>Platform</h4>
+        <p>{userAgentData.platform}</p>
       </header>
     </div>
   );
